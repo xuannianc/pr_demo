3c761bdc19195bdd4911c408f4d716173b93edec
CN\Adam Z Chen <chenxuannian@gmail.com>
2018-05-04 15:50:26 +0800

    1.update request form

--- common/request.py
+++ common/request.py
@@ -2,9 +2,190 @@ import cv2
 import numpy as np
 import pandas as pd
 import os
+from collections import Counter
+import re
+
+OFFSET = 30
+WORDS = Counter(["status",
+                 "payment",
+                 "no",
+                 "prepay",
+                 "description",
+                 "supplier",
+                 "name",
+                 "currency",
+                 "term",
+                 "requested",
+                 "date",
+                 "bu",
+                 "alternate",
+                 "payee"])
 
 
-def blur_and_split(filepath):
+def preprocessing(filepath):
+    media_dir = os.path.dirname(filepath)
+    img = cv2.imread(filepath)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    binary = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+    height, width = binary.shape
+    # 获取所有横线
+    k1 = cv2.getStructuringElement(cv2.MORPH_RECT, (width // 30, 1))
+    heroded = cv2.erode(binary, k1, iterations=1)
+    hdilated = cv2.dilate(heroded, k1, iterations=1)
+    # cv2.imshow('hdilated',hdilated)
+    # cv2.waitKey(0)
+    # 获取所有竖线
+    k2 = cv2.getStructuringElement(cv2.MORPH_RECT, (1, height // 30))
+    veroded = cv2.erode(binary, k2, iterations=1)
+    vdilated = cv2.dilate(veroded, k2, iterations=1)
+    # 合并所有紧连的横线
+    ## 横线上所有点的坐标
+    h_points = np.where(hdilated == 255)
+    ## 横线上所有点的 x 轴坐标
+    axis0 = pd.Series(h_points[0])
+    ## 横线上所有点的 x 轴坐标进行排序
+    h_statistics = axis0.value_counts().sort_index()
+    print(h_statistics)
+    h_pre_index = h_statistics.index[0]
+    all_h_index = [h_pre_index]
+    for index in h_statistics.index[1:]:
+        if index - h_pre_index <= 5:
+            ## 在 binary 图上合并
+            or_lines = binary[h_pre_index] | binary[index]
+            binary[index] = 0
+            binary[h_pre_index] = or_lines
+            ## 在 hdilated 图上合并，为了获取表格交点
+            or_lines = hdilated[h_pre_index] | hdilated[index]
+            hdilated[index] = 0
+            hdilated[h_pre_index] = or_lines
+        else:
+            h_pre_index = index
+            all_h_index.append(index)
+    print(all_h_index)
+    ## 竖线上所有点的坐标
+    v_points = np.where(vdilated == 255)
+    ## 竖线上所有点的 y 轴坐标
+    axis1 = pd.Series(v_points[1])
+    ## 竖线上所有点的 y 轴坐标进行排序
+    v_statistics = axis1.value_counts().sort_index()
+    v_pre_index = v_statistics.index[0]
+    all_v_index = [v_pre_index]
+    for index in v_statistics.index[1:]:
+        if index - v_pre_index <= 5:
+            ## 在 binary 图上合并
+            or_lines = binary[:, v_pre_index] | binary[:, index]
+            binary[:, index] = 0
+            binary[:, v_pre_index] = or_lines
+            ## 在 vdilated 图上合并
+            or_lines = vdilated[:, v_pre_index] | vdilated[:, index]
+            vdilated[:, index] = 0
+            vdilated[:, v_pre_index] = or_lines
+        else:
+            v_pre_index = index
+            all_v_index.append(index)
+    # 延长横线
+    for index in all_h_index:
+        col_arr, = np.where(hdilated[index] == 255)
+        min_col = np.min(col_arr)
+        max_col = np.max(col_arr)
+        hdilated[index, min_col - 5: max_col + 5] = 255
+    # 延长竖线
+    for index in all_v_index:
+        row_arr, = np.where(vdilated[:, index] == 255)
+        min_row = np.min(row_arr)
+        max_row = np.max(row_arr)
+        vdilated[min_row - 5:max_row + 5, index] = 255
+    # 完整表格
+    or_dilated = cv2.bitwise_or(vdilated, hdilated)
+    # 表格的交叉点
+    and_dilated = cv2.bitwise_and(vdilated, hdilated)
+    # 找出表格左上方顶点和左下方顶点
+    cross_points = np.where(and_dilated == 255)
+    axis0 = pd.Series(cross_points[0])
+    axis1 = pd.Series(cross_points[1])
+    h_statistics = axis0.value_counts().sort_index()
+    v_statistics = axis1.value_counts().sort_index()
+    ## 表格第一条和最后一条横线的下标
+    h_first = h_statistics.index[0]
+    h_last = h_statistics.index[-1]
+    ## 表格第一条和最后一条竖线的下标
+    v_first = v_statistics.index[0]
+    v_last = v_statistics.index[-1]
+    ## 表格第一条横线在所有横线中的 index
+    h_first_index = all_h_index.index(h_first)
+    ## 表格最后一条横线在所有横线中的 index
+    h_last_index = all_h_index.index(h_last)
+    ## 表格数据的行数
+    rows = h_last_index - 1 - (h_first_index + 1)
+    # 处理表格上方内容, part1
+    for index in all_h_index[:h_first_index]:
+        ## 删除所有横线
+        binary[index] = 0
+    ## 截取文件,part1
+    part1 = binary[:h_first - OFFSET, :]
+    part1 = cv2.bitwise_not(part1)
+    blurred_part1 = cv2.GaussianBlur(part1, (3, 3), 0)
+    cv2.imwrite(os.path.join(media_dir, 'blurred_part1.jpg'), blurred_part1)
+    request_part1 = binary[:h_first - OFFSET, :]
+    request_part1 = cv2.bitwise_not(request_part1)
+    cv2.imwrite(os.path.join(media_dir, 'request_part1.jpg'), request_part1)
+    # 处理表格下方内容, part3
+    for index in all_h_index[h_last_index + 1:]:
+        ## 删除所有横线
+        binary[index] = 0
+    ## 截取文件,part3
+    part3 = binary[h_last + OFFSET:all_h_index[-1] + OFFSET, :]
+    part3 = cv2.bitwise_not(part3)
+    blurred_part3 = cv2.GaussianBlur(part3, (3, 3), 0)
+    cv2.imwrite(os.path.join(media_dir, 'blurred_part3.jpg'), blurred_part3)
+    request_part3 = binary[h_last + OFFSET:all_h_index[-1] + OFFSET, :]
+    request_part3 = cv2.bitwise_not(request_part3)
+    cv2.imwrite(os.path.join(media_dir, 'request_part3.jpg'), request_part3)
+    request_part2 = gray[h_first - 1:h_last + 1, :]
+    cv2.imwrite(os.path.join(media_dir, 'request_part2.jpg'), request_part2)
+    # 处理表格
+    ## 处理横线
+    for index in all_h_index[h_last_index - 1:h_first_index:-1]:
+        ### 去掉横线
+        binary[index] = 0
+        ### 下方填充空白行
+        binary = np.insert(binary, [index + 1] * OFFSET, 0, axis=0)
+        ### 上方填充空白行
+        binary = np.insert(binary, [index - 1] * OFFSET, 0, axis=0)
+    ## 处理竖线
+    for index in all_v_index[::-1]:
+        ### 去掉竖线
+        binary[:, index] = 0
+        ### 右方填充空白列
+        binary = np.insert(binary, [index + 1] * OFFSET, 0, axis=1)
+        ### 左方填充空白列
+        binary = np.insert(binary, [index - 1] * OFFSET, 0, axis=1)
+
+    col01 = binary[
+            all_h_index[h_first_index + 1] + OFFSET + 1:all_h_index[h_last_index - 1] + OFFSET * (2 * rows + 1) - 1,
+            all_v_index[0] + OFFSET:all_v_index[2] + OFFSET * (2 * 2 + 1)]
+    col2 = binary[
+           all_h_index[h_first_index + 1] + OFFSET + 1:all_h_index[h_last_index - 1] + OFFSET * (2 * rows + 1) - 1,
+           all_v_index[2] + OFFSET * (2 * 2 + 1):all_v_index[3] + OFFSET * (3 * 2 + 1)]
+    col3456 = binary[
+              all_h_index[h_first_index + 1] + OFFSET + 1:all_h_index[h_last_index - 1] + OFFSET * (2 * rows + 1) - 1,
+              all_v_index[3] + OFFSET * (3 * 2 + 1):all_v_index[7] + OFFSET * (7 * 2 + 1)]
+    col01 = cv2.bitwise_not(col01)
+    blurred_col01 = cv2.GaussianBlur(col01, (3, 3), 0)
+    cv2.imwrite(os.path.join(media_dir, 'blurred_col01.jpg'), blurred_col01)
+    cv2.imwrite(os.path.join(media_dir, 'col01.jpg'), col01)
+    col2 = cv2.bitwise_not(col2)
+    blurred_col2 = cv2.GaussianBlur(col2, (3, 3), 0)
+    cv2.imwrite(os.path.join(media_dir, 'blurred_col2.jpg'), blurred_col2)
+    cv2.imwrite(os.path.join(media_dir, 'col2.jpg'), col2)
+    col3456 = cv2.bitwise_not(col3456)
+    blurred_col3456 = cv2.GaussianBlur(col3456, (3, 3), 0)
+    cv2.imwrite(os.path.join(media_dir, 'blurred_col3456.jpg'), blurred_col3456)
+    cv2.imwrite(os.path.join(media_dir, 'col3456.jpg'), col3456)
+    return h_last_index - h_first_index - 2
+
+
+def preprocessing2(filepath):
     media_dir = os.path.dirname(filepath)
     img = cv2.imread(filepath)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -18,48 +199,187 @@ def blur_and_split(filepath):
     k2 = cv2.getStructuringElement(cv2.MORPH_RECT, (1, height // 30))
     veroded = cv2.erode(binary, k2, iterations=1)
     vdilated = cv2.dilate(veroded, k2, iterations=1)
-    # 获取竖线和横线交叉点
-    cross_point = cv2.bitwise_and(vdilated, hdilated)
-    # 合并竖线和横线
-    or_dilated = cv2.bitwise_or(vdilated, hdilated)
-    # 去掉竖线和横线
-    result = cv2.bitwise_xor(binary, or_dilated)
-    # 颜色取反
-    result_inv = cv2.bitwise_not(result)
-    # 滤波
-    blur = cv2.GaussianBlur(result_inv, (3, 3), 0)
-    # 先找第 0 条竖线和第 0 条横线的交点的坐标
-    point_array = np.where(cross_point == 255)
-    axis0 = pd.Series(point_array[0])
+    # 合并所有紧连的横线
+    ## 横线上所有点的坐标
+    h_points = np.where(hdilated == 255)
+    ## 横线上所有点的 x 轴坐标
+    axis0 = pd.Series(h_points[0])
+    ## 横线上所有点的 x 轴坐标进行排序
     h_statistics = axis0.value_counts().sort_index()
-    all_h = []
+    print(h_statistics)
     h_pre_index = h_statistics.index[0]
-    all_h.append(h_pre_index)
-    for index in h_statistics.index:
-        if index - h_pre_index > 5:
-            all_h.append(index)
+    all_h_index = [h_pre_index]
+    for index in h_statistics.index[1:]:
+        if index - h_pre_index <= 5:
+            ## 在 gray 图上合并
+            or_lines = gray[h_pre_index] & gray[index]
+            gray[index] = 255
+            gray[h_pre_index] = or_lines
+            ## 在 binary 图上合并
+            or_lines = binary[h_pre_index] & binary[index]
+            binary[index] = 0
+            binary[h_pre_index] = or_lines
+            ## 在 hdilated 图上合并，为了获取表格交点
+            or_lines = hdilated[h_pre_index] | hdilated[index]
+            hdilated[index] = 0
+            hdilated[h_pre_index] = or_lines
+        else:
             h_pre_index = index
-    all_v = []
-    axis1 = pd.Series(point_array[1])
+            all_h_index.append(index)
+    print(all_h_index)
+    ## 竖线上所有点的坐标
+    v_points = np.where(vdilated == 255)
+    ## 竖线上所有点的 y 轴坐标
+    axis1 = pd.Series(v_points[1])
+    ## 竖线上所有点的 y 轴坐标进行排序
     v_statistics = axis1.value_counts().sort_index()
-    print(v_statistics)
     v_pre_index = v_statistics.index[0]
-    all_v.append(v_pre_index)
-    for index in v_statistics.index:
-        if index - v_pre_index > 5:
-            all_v.append(index)
+    all_v_index = [v_pre_index]
+    for index in v_statistics.index[1:]:
+        if index - v_pre_index <= 5:
+            ## 在 gray 图上合并
+            or_lines = gray[:, v_pre_index] & gray[:, index]
+            gray[:, index] = 255
+            gray[:, v_pre_index] = or_lines
+            ## 在 binary 图上合并
+            or_lines = binary[:, v_pre_index] & binary[:, index]
+            binary[:, index] = 0
+            binary[:, v_pre_index] = or_lines
+            ## 在 vdilated 图上合并
+            or_lines = vdilated[:, v_pre_index] | vdilated[:, index]
+            vdilated[:, index] = 0
+            vdilated[:, v_pre_index] = or_lines
+        else:
             v_pre_index = index
-    print(all_v)
-    space = np.zeros((all_h[-2] - all_h[0], 20))
-    space[:, :] = 255
+            all_v_index.append(index)
+    # 延长横线
+    for index in all_h_index:
+        col_arr, = np.where(hdilated[index] == 255)
+        min_col = np.min(col_arr)
+        max_col = np.max(col_arr)
+        hdilated[index, min_col - 5: max_col + 5] = 255
+    # 延长竖线
+    for index in all_v_index:
+        row_arr, = np.where(vdilated[:, index] == 255)
+        min_row = np.min(row_arr)
+        max_row = np.max(row_arr)
+        vdilated[min_row - 5:max_row + 5, index] = 255
+    # 完整表格
+    or_dilated = cv2.bitwise_or(vdilated, hdilated)
+    # 表格的交叉点
+    and_dilated = cv2.bitwise_and(vdilated, hdilated)
+    # 找出表格左上方顶点和左下方顶点
+    cross_points = np.where(and_dilated == 255)
+    axis0 = pd.Series(cross_points[0])
+    axis1 = pd.Series(cross_points[1])
+    h_statistics = axis0.value_counts().sort_index()
+    v_statistics = axis1.value_counts().sort_index()
+    ## 表格第一条和最后一条横线的下标
+    h_first = h_statistics.index[0]
+    h_last = h_statistics.index[-1]
+    ## 表格第一条和最后一条竖线的下标
+    v_first = v_statistics.index[0]
+    v_last = v_statistics.index[-1]
+    ## 表格第一条横线在所有横线中的 index
+    h_first_index = all_h_index.index(h_first)
+    ## 表格最后一条横线在所有横线中的 index
+    h_last_index = all_h_index.index(h_last)
+    ## 表格数据的行数
+    rows = h_last_index - 1 - (h_first_index + 1)
+    # 处理表格上方内容, part1
+    for index in all_h_index[:h_first_index]:
+        ## 删除所有横线
+        gray[index] = 255
+    ## 截取文件,part1
+    request_part1 = gray[:h_first - OFFSET, :]
+    cv2.imwrite(os.path.join(media_dir, 'request_part1.jpg'), request_part1)
+    # 处理表格下方内容, part3
+    for index in all_h_index[h_last_index + 1:]:
+        ## 删除所有横线
+        gray[index] = 255
+    ## 截取文件,part3
+    request_part3 = gray[h_last + OFFSET:all_h_index[-1] + OFFSET, :]
+    cv2.imwrite(os.path.join(media_dir, 'request_part3.jpg'), request_part3)
+    request_part2 = gray[h_first - 1:h_last + 1, :]
+    cv2.imwrite(os.path.join(media_dir, 'request_part2.jpg'), request_part2)
+    # 处理表格
+    ## 处理横线
+    for index in all_h_index[h_last_index - 1:h_first_index:-1]:
+        ### 去掉横线
+        gray[index] = 255
+        ### 下方填充空白行
+        gray = np.insert(gray, [index + 1] * OFFSET, 255, axis=0)
+        ### 上方填充空白行
+        gray = np.insert(gray, [index - 1] * OFFSET, 255, axis=0)
+    ## 处理竖线
+    for index in all_v_index[::-1]:
+        ### 去掉竖线
+        gray[:, index] = 255
+        ### 右方填充空白列
+        gray = np.insert(gray, [index + 1] * OFFSET, 255, axis=1)
+        ### 左方填充空白列
+        gray = np.insert(gray, [index - 1] * OFFSET, 255, axis=1)
 
-    cv2.imwrite(os.path.join(media_dir, 'part1.jpg'), blur[:all_h[0] - 1, :])
-    cv2.imwrite(os.path.join(media_dir, 'part2.jpg'), blur[all_h[0] + 1:all_h[-1] - 1, :])
-    cv2.imwrite(os.path.join(media_dir, 'part3.jpg'), blur[all_h[-1] + 1:, :])
+    ## 为表格的每个列创建图片文件
+    for i in range(7):
+        request_col = gray[all_h_index[
+                               h_first_index + 1] + OFFSET + 1: all_h_index[h_last_index - 1] + OFFSET * (
+                2 * rows + 1) - 1,
+                      all_v_index[i] + OFFSET * (2 * i + 1): all_v_index[i + 1] + OFFSET * (2 * (i + 1) + 1) - 1]
+        cv2.imwrite(os.path.join(media_dir, 'request_col' + str(i) + '.jpg'), request_col)
+    return h_last_index - h_first_index - 2
 
-    for i in range(len(all_v) - 1):
-        col = blur[all_h[1]:all_h[-2], all_v[i] + 1:all_v[i + 1]]
-        col_num = col.shape[1]
-        col_with_left_space = np.insert(col, [3] * 20, values=255, axis=1)
-        col_with_space = np.insert(col_with_left_space, [col_num + 20 - 3] * 20, values=255, axis=1)
-        cv2.imwrite(os.path.join(media_dir, 'col' + str(i) + '.jpg'), col_with_space)
+
+def P(word, N=sum(WORDS.values())):
+    "Probability of `word`."
+    return WORDS[word] / N
+
+
+def correction(word):
+    "Most probable spelling correction for word."
+    # 只处理长度大于 2 的字符串
+    if len(word) > 2:
+        return max(candidates(word), key=P)
+    else:
+        return word
+
+
+def candidates(word):
+    "Generate possible spelling corrections for word."
+    return (known([word]) or known(edits1(word)) or known(edits2(word)) or [word])
+
+
+def known(words):
+    "The subset of `words` that appear in the dictionary of WORDS."
+    return set(w for w in words if w in WORDS)
+
+
+def edits1(word):
+    "All edits that are one edit away from `word`."
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+    # 把单词拆分成两段
+    splits = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+    # print('splits:', splits)
+    # print(''.join(['-'] * 50))
+    # 删除一个字母
+    deletes = [L + R[1:] for L, R in splits if R]
+    # print('deletes', deletes)
+    # print(''.join(['-'] * 50))
+    # 相连两个字母交换位置
+    transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R) > 1]
+    # print('transposes', transposes)
+    # print(''.join(['-'] * 50))
+    # 把每一个字母分别替换成其他 25 个字母
+    replaces = [L + c + R[1:] for L, R in splits if R for c in letters]
+    # print('replaces', replaces)
+    # print(''.join(['-'] * 50))
+    inserts = [L + c + R for L, R in splits for c in letters]
+    # 在每一个位置分别插入 26 个字母中的一个
+    # print('inserts', inserts)
+    # print(''.join(['-'] * 50))
+    return set(deletes + transposes + replaces + inserts)
+
+
+def edits2(word):
+    "All edits that are two edits away from `word`."
+    return (e2 for e1 in edits1(word) for e2 in edits1(e1))
--- request/views.py
+++ request/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 from aip import AipOcr
-from common.request import blur_and_split
+from common.request import preprocessing, preprocessing2, correction
 from django.http.response import JsonResponse
 import os
 import re
@@ -15,6 +15,9 @@ API_KEY = '6KUoA2v1eVXRAIcVXnOtrwlA'
 SECRET_KEY = 'bMGaeRNO2fsPS0Tpl7HozcGDclvQByYm'
 client = AipOcr(APP_ID, API_KEY, SECRET_KEY)
 
+PART1_FIELDS = ['payment no', 'prepay', 'description', 'supplier', 'supplier name', 'currency', 'vat',
+                'payment term', 'bu', 'requested date', 'alternate payee']
+
 
 def index(request):
     return render(request, 'request/index.html')
@@ -25,87 +28,99 @@ def get_file_content(filePath):
         return fp.read()
 
 
+def is_part1_field(words):
+    # 没有数字
+    if not re.search(r'\d', words):
+        corrected_word_list = []
+        for word in words.split(' '):
+            corrected_word = correction(word)
+            corrected_word_list.append(corrected_word)
+        corrected_words = ' '.join(corrected_word_list)
+        if corrected_words in PART1_FIELDS:
+            return corrected_words, True
+        else:
+            return None, False
+    return None, False
+
+
 @csrf_exempt
 def upload(request):
     file = request.FILES['file']
     fs = FileSystemStorage()
     filename = fs.save(file.name, file)
     filepath = fs.path(filename)
-    blur_and_split(filepath)
+    row_num = preprocessing2(filepath)
     # print(fs.url(filename))
     media_dir = os.path.dirname(filepath)
     # 处理 part1
-    part1_path = os.path.join(media_dir, 'part1.jpg')
+    part1_path = os.path.join(media_dir, 'request_part1.jpg')
     image = get_file_content(part1_path)
-    """ 调用通用文字识别, 图片参数为本地图片 """
     part1_scan_result = client.basicGeneral(image)
     all_words = []
     for item in part1_scan_result['words_result']:
         all_words.append(item['words'])
-
-    all_fields = ['Payment No', 'Prepay', 'Description', 'Supplier', 'Supplier Name', 'Currency', 'VAT', 'Payment Term',
-                  'Requested Date', 'BU', 'Alternate Payee']
+    print(all_words)
+    # all_fields = ['Payment No', 'Prepay', 'Description', 'Supplier', 'Supplier Name', 'Currency', 'VAT', 'Payment Term',
+    #               'Requested Date', 'BU', 'Alternate Payee']
     part1_result = {}
     for idx, words in enumerate(all_words):
-        if words in all_fields:
-            if not all_words[idx + 1] in all_fields:
-                part1_result[words] = all_words[idx + 1]
+        corrected_words, is_true = is_part1_field(words)
+        if is_true:
+            if idx < len(all_words) - 1:
+                corrected_words2, is_true2 = is_part1_field(all_words[idx + 1])
+                if is_true2:
+                    part1_result[corrected_words] = ''
+                else:
+                    part1_result[corrected_words] = all_words[idx + 1]
             else:
-                part1_result[words] = ''
-    # 处理 part2
-    df = pd.DataFrame(columns=['PO No', 'Sub-Brand Description', 'PO Description', 'Category', 'PO Payment Amount',
-                               'Deductable Tax Amount', 'Invoice Amount'])
-    row_num = None
-    for i in range(2):
-        image = get_file_content(os.path.join(media_dir, 'col' + str(i) + ".jpg"))
-        result = client.basicGeneral(image)
-        all_words = []
-        for item in result['words_result']:
-            all_words.append(item['words'])
-        row_num = len(all_words)
-        df[df.columns[i]] = all_words
-
-    image = get_file_content(os.path.join(media_dir, "col2.jpg"))
-    result = client.basicGeneral(image)
-    all_words = []
-    all_description = []
-    for item in result['words_result']:
-        all_words.append(item['words'])
-    splits = np.array_split(np.array(all_words), row_num)
-    for split in splits:
-        all_description.append(' '.join(split.tolist()))
-    df[df.columns[2]] = all_description
-
-    for i in range(3, 7):
-        image = get_file_content(os.path.join(media_dir, 'col' + str(i) + ".jpg"))
-        result = client.basicGeneral(image)
-        all_words = []
-        for item in result['words_result']:
-            all_words.append(item['words'])
-        df[df.columns[i]] = all_words
-    # 处理 part3
-    part3_path = os.path.join(media_dir, 'part3.jpg')
-    image = get_file_content(part3_path)
-    """ 调用通用文字识别, 图片参数为本地图片 """
-    part3_scan_result = client.basicGeneral(image)
-    all_words = []
-    for item in part3_scan_result['words_result']:
-        all_words.append(item['words'])
-    part3_result = {}
-    for idx, words in enumerate(all_words):
-        if re.match(r'^[Ii]?nput', words):
-            part3_result['Input By'] = all_words[idx + 1]
-        elif re.match(r'^[Bb]?udget', words):
-            part3_result['Budget Owner'] = all_words[idx + 1]
-        elif re.match(r'^[Ss]?tarted', words):
-            part3_result['Started By'] = all_words[idx + 1]
-        elif re.match(r'^[Aa]?pproved', words):
-            part3_result['Approved By'] = all_words[idx + 1]
-        elif re.match(r'^[Pp]?ending', words):
-            part3_result['Pending By'] = all_words[idx + 1]
-    return JsonResponse(data={'part1_result': part1_result,
-                              'part2_result': df.to_dict(),
-                              'part3_result': part3_result})
+                part1_result[corrected_words] = ''
+    # # 处理 part2
+    # df = pd.DataFrame(columns=['PO No', 'Sub-Brand Description', 'PO Description', 'Category', 'PO Payment Amount',
+    #                            'Deductable Tax Amount', 'Invoice Amount'])
+    # ## column 0,1,3,4,5,6
+    # for i in [j for j in range(7) if j != 2]:
+    #     col_image = get_file_content(os.path.join(media_dir, "request_col" + str(i) + ".jpg"))
+    # col_scan_result = client.basicGeneral(col_image)
+    # all_words = []
+    # for item in col_scan_result['words_result']:
+    #     all_words.append(item['words'])
+    # ### 补全未扫描的字段
+    # if col_scan_result['words_result_num'] == row_num:
+    #     all_words.extend([None] * (row_num - col_scan_result['words_result_num']))
+    # df[df.columns[i]] = all_words
+    # ## column 2
+    # col2_image = get_file_content(os.path.join(media_dir, "request_col2.jpg"))
+    # col2_scan_result = client.basicGeneral(col2_image)
+    # all_words = []
+    # all_description = []
+    # for item in col2_scan_result['words_result']:
+    #     all_words.append(item['words'])
+    # splits = np.array_split(np.array(all_words), row_num)
+    # for split in splits:
+    #     all_description.append(' '.join(split.tolist()))
+    # df[df.columns[2]] = all_description
+    # # 处理 part3
+    # part3_path = os.path.join(media_dir, 'request_part3.jpg')
+    # part3_image = get_file_content(part3_path)
+    # part3_scan_result = client.basicGeneral(part3_image)
+    # all_words = []
+    # for item in part3_scan_result['words_result']:
+    #     all_words.append(item['words'])
+    # part3_result = {}
+    # for idx, words in enumerate(all_words):
+    #     if re.match(r'^[Ii]?nput', words):
+    #         part3_result['Input By'] = all_words[idx + 1]
+    #     elif re.match(r'^[Bb]?udget', words):
+    #         part3_result['Budget Owner'] = all_words[idx + 1]
+    #     elif re.match(r'^[Ss]?tarted', words):
+    #         part3_result['Started By'] = all_words[idx + 1]
+    #     elif re.match(r'^[Aa]?pproved', words):
+    #         part3_result['Approved By'] = all_words[idx + 1]
+    #     elif re.match(r'^[Pp]?ending', words):
+    #         part3_result['Pending By'] = all_words[idx + 1]
+    return JsonResponse(data={'part1_result': part1_result})
+    # 'part2_result': df.to_dict(),
+    # 'part3_result': part3_result})
 
 
 def handle_uploaded_file(f):
